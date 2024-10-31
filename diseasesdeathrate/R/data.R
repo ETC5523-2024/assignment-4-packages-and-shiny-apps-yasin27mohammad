29a10e721b54096ad42aad81d2e5d758dd047604
Mohammad Yasin <iam.yasin27@gmail.com>
2024-10-31 16:39:44 +1100

    add the data.R in the R file

---
+++ diseasesdeathrate/R/data.R
@@ -0,0 +1,12 @@
+#' Age-standardized Death Rates from Infectious and Parasitic Diseases in Asian Countries
+#'
+#' @format ## 'infectious_parasitic_diseases_death_rate'
+#' A data frame with 738 rows and 4 columns
+#' \describe{
+#'   \item{Entity}{Country name, representing the Asian country in which the data was recorded}
+#'   \item{Code}{Country code, which is the ISO code for each country}
+#'   \item{Year}{The year in which the data was recorded}
+#'   \item{death rate per 100,000 population}{Age-standardized death rate per 100,000 population due to infectious and parasitic diseases}
+#' }
+#' @source <https://ourworldindata.org/grapher/infectious-and-parasitic-diseases-death-rate-who-mdb>
+"infectious_parasitic_diseases_death_rate"
